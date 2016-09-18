3cdec8a6c0b7aad42cf565d60ec2bec276d60faa
Fevzi Ozgul <fevzi.ozgul@devfactory.com>
2016-09-18 14:27:53 +0300

    diff

--- problem_solver.rb
+++ problem_solver.rb
@@ -1,1 +1,1 @@
- class ProblemSolver	def solve_easy_problem		end	 end
+ class ProblemSolver	def solve_easy_problem		end		def solve_hard_problem		end	def solve_tough_problem		end	 end
